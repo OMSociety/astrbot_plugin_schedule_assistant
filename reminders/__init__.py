1e0ca2b06510d57da8198b32412f104ba1cbb0a9
山海学社OMSociety <85862362+OMSociety@users.noreply.github.com>
2026-04-22 05:54:25 +0800

    v1.8.0: reminders/__init__.py + schedule.py

--- reminders/__init__.py
+++ reminders/__init__.py
@@ -3,5 +3,6 @@ from .bath import BathReminder
 from .sleep import SleepReminder
 from .water import WaterReminder
 from .briefing import BriefingReminder
+from .schedule import ScheduleReminder, check_and_trigger_schedule_reminder
 
-__all__ = ["BathReminder", "SleepReminder", "WaterReminder", "BriefingReminder"]
+__all__ = ["BathReminder", "SleepReminder", "WaterReminder", "BriefingReminder", "ScheduleReminder", "check_and_trigger_schedule_reminder"]
